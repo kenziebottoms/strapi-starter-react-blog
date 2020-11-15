c7ff68c55955e2ec69731acc21bc2052769a0513
Kenzie Bottoms <kenziebottoms@gmail.com>
2020-11-14 19:02:40 -0600

    fix process env, install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "globals": {
+        "process": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2020,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+}
--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,14 +1,17 @@
-import { ApolloClient } from "apollo-client";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { HttpLink } from "apollo-link-http";
+import { ApolloClient } from "apollo-client"
+import { InMemoryCache } from "apollo-cache-inmemory"
+import { HttpLink } from "apollo-link-http"
+import dotenv from "dotenv"
 
-const cache = new InMemoryCache();
+dotenv.config()
+
+const cache = new InMemoryCache()
 const link = new HttpLink({
-  uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`
-});
+    uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`
+})
 const client = new ApolloClient({
-  cache,
-  link
-});
+    cache,
+    link
+})
 
-export default client;
+export default client
